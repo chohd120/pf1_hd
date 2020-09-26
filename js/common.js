d47044dc7f20cda7bcc9da3d67240ec44adf5f05
chohd120 <68522337+chohd120@users.noreply.github.com>
2020-09-26 21:31:36 +0900

    popup btnBox error

--- js/common.js
+++ js/common.js
@@ -69,23 +69,12 @@
     }
   }
 
-  $(".slideInner").slick({
-    autoplay: true, // 자동재생
-    autoplaySpeed: 3000, // 간격시간
-    dots: true, // 동그라미버튼
-    speed: 600, // 바뀌는시간(생략가능)
-    slidesToShow: 1, // 보여질슬라이드수(생략가능)
-    slidesToScroll: 1, // 이동슬라이드수(생략가능)
-    pauseOnHover: true, // 마우스오버시 멈춤여부(생략가능)
-    pauseOnDotsHover: true, // 동그라미번호버튼에 호버시 멈춤여부(생략가능)
-    pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
-    cssEase: "linear", // 속도함수(생략가능)
-    draggable: true, // 마우스드래그시 슬라이드 교체가능여부(생략가능)
-    fade: false, // 슬라이드가 수평으로 이동하지 않고, 제자리에서 사라지고 나타남(생략가능)
-    arrows: true, // 좌우화살표 사용여부(생략가능)
-    prevArrow:
-      '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
-    nextArrow:
-      '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
-  });
+  //윈도우 팝업
+  if ($.cookie('pop') != 'none') {
+    window.open('popup.html', '창제목', 'top=100, left=200, width=660, height=720') // 괄호안 옵션 필수 입력, 창제목은 표시되지않음, 사이즈를 없애면 기본 윈도우 크기
+  }
+
+
+
+
 })(jQuery);
